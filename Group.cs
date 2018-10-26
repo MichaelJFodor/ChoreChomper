ce18a134531645a57661f6429b084c4b29938bf7
scuPulos <44333074+scuPulos@users.noreply.github.com>
2018-10-25 20:41:16 -0700

    Initial commit
    Created a basic layout of things like main classes for the MVP. 
    As far as testing/interface goes it just shows the name of the most recent chore you entered.

---
+++ Group.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UserSpace;
+using TaskSpace;
+using ChoreSpace;
+
+namespace GroupSpace
+{
+    class Group
+    {
+        int groupId;
+        string groupName;
+        bool valid;
+
+        List<User> users;
+        TaskList tasks;
+
+        public Group()
+        {
+            groupId = 0;
+            groupName = "Fake";
+            valid = false;
+        }
+
+        public bool AssignGroup(int groupId)
+        {
+            //TODO:
+            //Fetch Grouup data corresponding to groupId
+            //Assign data values to group
+            valid = true;
+            //return false if group was not created or true if it was
+            return false;
+        }
+
+        public bool AssignGroup(string groupName)
+        {
+            //TODO:
+            //Fetch Grouup data corresponding to groupName
+            //Assign data values to group
+            valid = true;
+            //return false if group was not created or true if it was
+            return false;
+        }
+        
+        public Group GenerateNew(string Name)
+        {
+            //Add a new group to the database with name Name
+            //Add the local user to the group's list of users
+            return (new Group());
+        }
+
+        public Group GenerateTestGroup()
+        {
+            groupId = 0;
+            groupName = "Best_Group";
+            valid = true;
+            users = new List<User>();
+            tasks = new TaskList();
+            return this;
+        }
+
+        public User AddUser(User passedUser)
+        {
+            users.Add(passedUser);
+            return (passedUser);
+        }
+
+        public Chore AddChore(Chore chore)
+        {
+            tasks.AddChore(chore);
+            return chore;
+        }
+
+        public TaskList GetTaskList()
+        {
+            return(tasks);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,38 @@
+﻿namespace UserSpace
+{
+    class User
+    {
+        int userId;
+        string userName;
+
+        public User()
+        {
+            //TODO: creating new users
+        }
+
+        public User(string name)
+        {
+            userName = name;
+            //TODO: fetch and check id in database
+        }
+
+        public bool CheckCredentials(string pass)
+        {
+            //TODO: compare against the databases' password for this userId
+            return false;
+        }
+
+        public int GetId()
+        {
+            return userId;
+        }
+
+        public User GenerateTestUser()
+        {
+            userId = 0;
+            userName = "Bob";
+
+            return this;
+        }
+    }
+}

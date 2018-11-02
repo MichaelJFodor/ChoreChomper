0d07e72509ac64d0fdb253a8b613bab25d66a4f6
Michael Fodor <mfodor97@gmail.com>
2018-11-02 10:18:46 -0700

    Add files via upload
    This is incomplete.
    Still deciding between pages vs activities vs fragments.
    Seems that each has their time and place but each needs more research for which is best for the program.
    As of right now, we are implementing activities, but may change based off of the needs of the navigational system.
    
    
    This is incomplete because the research went initially into PAGES and not ACTIVITIES. I will update the code to fit the needs of the group.

---
+++ Chores.cs
@@ -0,0 +1,87 @@
+using ChoreChomper.Model.Utility;
+
+namespace ChoreChomper.Model
+{
+    public class Chore
+    {
+        int choreId;
+        string choreName;
+        int assignedUserId;
+        int completedUserId;
+        Timestamp deadlineTimestamp;
+        Timestamp completedTimestamp;
+        bool isPriority;
+        bool isCompleted;
+
+        //constructor
+        public Chore(string name, int passedUserId, string deadline)
+        {
+            isCompleted = false;
+            completedUserId = -1;
+            completedTimestamp = new Timestamp();
+
+            choreName = name;
+            choreId = generateChoreId();
+            assignedUserId = passedUserId;
+            deadlineTimestamp = new Timestamp(deadline);
+        }
+
+        int generateChoreId()
+        {
+            //TODO: fetch greatest chore id and increment by 1
+            return 0;
+        }
+
+        public void MarkPriority()
+        {
+            isPriority = true;
+        }
+
+        public void MarkComplete()
+        {
+            //TODO: get the Id of the user who marked it complete and assign it
+            completedUserId = assignedUserId;
+            completedTimestamp = new Timestamp().CurrentTimestamp();
+            isCompleted = true;
+        }
+
+        public int AssignUser(int passedUserId)
+        {
+            assignedUserId = passedUserId;
+            return (assignedUserId);
+        }
+
+        public Timestamp UpdateDeadline(string deadline)
+        {
+            deadlineTimestamp = new Timestamp(deadline);
+            return deadlineTimestamp;
+        }
+
+        public bool CheckId(int keyId)
+        {
+            return (keyId == choreId);
+        }
+        
+        public string GetName()
+        {
+            return (choreName);
+        }
+
+        private Button btnBack;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.SecondActivity);
+            btnBack = FindViewById<Button>(Resource.Id.btnBack);
+            btnBack.Click += btnBack_Click;
+        }
+
+        private void btnBack_Click(object senser, EventArgs e)
+        {
+            StartActivity(typeof(MainActivity));
+            
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,85 @@
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Runtime;
+using Android.Widget;
+using Android.Views;
+
+using ChoreChomper.ViewControllers;
+using ChoreChomper.Model;
+
+namespace ChoreChomper
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        ChoreChomper.ViewControllers.Controller controller;
+        ChoreChomper.Model.User user;
+        ChoreChomper.Model.Group group;
+        public User getMainUser() { return user; }
+        public Group getMainGroup() { return group; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // TODO: should likely be done in the loginLayoutController
+            SetupUserData();
+
+            // For future reference, remeber to set the content view before assigning the controller
+            SetContentView(Resource.Layout.loginLayout);
+            controller = new LoginController(this);
+        }
+        
+        private void SetupUserData()
+        {
+            user = new User().GenerateTestUser();
+            group = new Group().GenerateTestGroup();
+            group.AddUser(user);
+        }
+
+        public bool ChangeTo(int layout)
+        {
+            if (layout == Resource.Layout.activity_main)
+            {
+                SetContentView(Resource.Layout.activity_main);
+                controller = new HomeController(this);
+                return true;
+            }
+            else if (layout == Resource.Layout.choreListLayout)
+            {
+                SetContentView(Resource.Layout.choreListLayout);
+                controller = new ChoreListController(this);
+                return true;
+            }
+            else if(layout == Resource.Layout.loginLayout)
+            {
+                SetContentView(Resource.Layout.loginLayout);
+                controller = new LoginController(this);
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+        private Button nextPageButton;
+
+        protected void onCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            nextPageButton = FindViewById<Button>(Resource.Id.nextPageButton);
+
+            nextPageButton.Click += nextPageButton_Click;
+        }
+
+        private void nextPageButton_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(SecondActivity));
+        }
+    }
+}

09f6a9bdd032a5087bc71f726508b8c50cbaf855
Michael Fodor <mfodor97@gmail.com>
2018-11-07 17:24:22 -0800

    Add files via upload
    Added a function in group to find the chores due the active week

---
+++ Group.cs
@@ -0,0 +1,88 @@
+   public class Group
+    {
+        int groupId;
+        string groupName;
+        bool valid;
+
+        List<User> users;
+        TaskList tasks;
+
+        public Group()
+        {
+            groupId = 0;
+            groupName = "Fake";
+            valid = false;
+        }
+
+        public bool AssignGroup(int groupId)
+        {
+            //TODO:
+            //Fetch Grouup data corresponding to groupId
+            //Assign data values to group
+            valid = true;
+            //return false if group was not created or true if it was
+            return false;
+        }
+
+        public bool AssignGroup(string groupName)
+        {
+            //TODO:
+            //Fetch Grouup data corresponding to groupName
+            //Assign data values to group
+            valid = true;
+            //return false if group was not created or true if it was
+            return false;
+        }
+
+        public Group GenerateNew(string Name)
+        {
+            //Add a new group to the database with name Name
+            //Add the local user to the group's list of users
+            return (new Group());
+        }
+        
+		public Group GenerateTestGroup()
+        {
+            groupId = 0;
+            groupName = "Best_Group";
+            valid = true;
+            users = new List<User>();
+            tasks = new TaskList();
+            return this;
+        }
+
+        public User AddUser(User passedUser)
+        {
+            users.Add(passedUser);
+            return (passedUser);
+        }
+
+        public Chore AddChore(Chore chore)
+        {
+            tasks.AddChore(chore);
+            return chore;
+        }
+
+        public TaskList GetTaskList()
+        {
+            return (tasks);
+        }
+
+        public List<string> GetChoresDueThisWeek()
+        {
+            List<string> tasks = new List<string>();
+            TaskList choreList = GetTaskList();
+            Timestamp time = new Timestamp();
+            foreach (Chore chore in choreList.GetChoreList())
+            {
+                // Check if within 7 days of current day
+                if (chore.GetDeadline().GetDay() >= time.CurrentTimestamp().GetDay() && chore.GetDeadline().GetDay() <= time.CurrentTimestamp().GetDay() + 7)
+                {
+                    tasks.Add(chore.GetName());
+                }
+            }
+
+            return tasks;
+        }
+	}
+    

44f2869de3d88d01dff1ec2c1e78dec926695db1
milelyk <44346861+milelyk@users.noreply.github.com>
2018-11-27 08:53:00 -0800

    Update completeChore.php

--- completeChore.php
+++ completeChore.php
@@ -4,7 +4,7 @@
 	$cid = $_GET["ChoreId"];
 	$time = $_GET["TimeStamp"];
 	$completed_by = $_GET["CompletedBy"];
-	$complete_chore = "UPDATE chorechomper.chores SET completed_by = '$completed_by', completed_date = '$time' WHERE (chore_id = '$cid')";
+	$complete_chore = "UPDATE chorechomper.chores SET completed_by = '$completed_by', completed_date = '$time' WHERE (c_ID = '$cid')";
 	if(mysqli_query($conn,$complete_chore))
 	{
 		echo 'success';

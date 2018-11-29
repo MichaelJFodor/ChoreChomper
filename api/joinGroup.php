cf5cda15a696010d4895757f491ca623f5e4639b
milelyk <44346861+milelyk@users.noreply.github.com>
2018-11-29 08:55:06 -0800

    Update joinGroup.php

--- api/joinGroup.php
+++ api/joinGroup.php
@@ -1,20 +1,26 @@
 <?php
+	//this file joins a group given the group parameters
 	include 'connection.php';
 	$conn = OpenCon();
+
 	$name = $_GET["Group_name"];
 	$password = $_GET["Group_password"];
 	$uid = $_GET["User_id"];
-	$sql = ("SELECT idGroup FROM chorechomper.group WHERE nameGroup = '$name' AND groupPassword = '$password'");
-	$result=mysqli_query($conn,$sql);
+	
+	$getGid_sql = ("SELECT idGroup FROM chorechomper.group WHERE nameGroup = '$name' AND groupPassword = '$password'");
+	$result=mysqli_query($conn,$getGid_sql);
 	$row = mysqli_fetch_assoc($result);
 	$gid = $row['idGroup'];
+
 	if($result->num_rows != 0)
 	{
-		$enter = "INSERT INTO chorechomper.group_has_users (id_group, user_id) VALUES ('$gid','$uid')";
-		mysqli_query($conn,$enter);
+		$enter_into_relational = "INSERT INTO chorechomper.group_has_users (id_group, user_id) VALUES ('$gid','$uid')";
+		mysqli_query($conn,$enter_into_relational);
 		echo json_encode($gid);
 	}
+	
 	else
 		echo json_encode('-1');
+	
 	CloseCon($conn);
 	?>

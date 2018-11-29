2e3616f33ed84e4389f1406a8515af76b59301c6
milelyk <44346861+milelyk@users.noreply.github.com>
2018-11-29 08:59:57 -0800

    Update updateChore.php

--- api/updateChore.php
+++ api/updateChore.php
@@ -1,21 +1,22 @@
 <?php
+	//this files updates the chores given the new data
 	include 'connection.php';
 	$conn = OpenCon();
+
 	$ChoreName_ = $_GET["ChoreName"];
 	$CompleteBy_ = $_GET["CompleteBy"];
 	$AssignedTo_ = $_GET["AssignedTo"];
 	$Priority_ = $_GET["Priority"];
 	$chore_id_ = $_GET["ChoreId"];
-	//$completed_by_person_ = $_GET["completed_by_person"];
-	//$completed_date_ = $_GET["completed_date"];
-	$sql = "UPDATE chores SET complete_by_date = '$CompleteBy_', chore_title = '$ChoreName_', priority = '$Priority_', assigned_to = '$AssignedTo_' WHERE (c_ID = '$chore_id_')";
-	if(mysqli_query($conn,$sql))
+	$updateChore_sql = "UPDATE chores SET complete_by_date = '$CompleteBy_', chore_title = '$ChoreName_', priority = '$Priority_', assigned_to = '$AssignedTo_' WHERE (c_ID = '$chore_id_')";
+	
+	if(mysqli_query($conn,$updateChore_sql))
 	{
 		echo json_encode('success');
 	}
+	
 	else
-	{
 		echo json_encode('-1');
-	}
+	
 	CloseCon($conn);
 ?>

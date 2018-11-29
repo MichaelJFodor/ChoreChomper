1d48800dc485e5515b43e3e002ce4b869aab1821
milelyk <44346861+milelyk@users.noreply.github.com>
2018-11-29 08:47:37 -0800

    Update insertnewchore.php

--- api/insertnewchore.php
+++ api/insertnewchore.php
@@ -1,32 +1,32 @@
 <?php 
+	//this files inserts new chores into the database
 	include 'connection.php';
 	$conn = OpenCon();
+
 	$ChoreName_ = $_GET["ChoreName"];
 	$CompleteBy_ = $_GET["CompleteBy"];
 	$AssignedTo_ = $_GET["AssignedTo"];
 	$Priority_ = $_GET["Priority"];
+	
+	//makes sure that the person who it is assigned to actually exists
 	$check = "SELECT user_id FROM chorechomper.user WHERE user_id = '$AssignedTo_'";
 	$u_result = mysqli_query($conn,$check);
 	$row = mysqli_fetch_assoc($u_result);
 	$uid = $row['user_id'];
+	
 	if($u_result->num_rows != 0)
 	{
-		echo 'start';
-		$sql = "INSERT into chores (chore_title, complete_by_date, assigned_to, priority) Values ('$ChoreName_', '$CompleteBy_', '$AssignedTo_', '$Priority_')";
-		if(mysqli_query($conn,$sql))
+		//inserts the new chore into the db
+		$insertchore_sql = "INSERT into chores (chore_title, complete_by_date, assigned_to, priority) Values ('$ChoreName_', '$CompleteBy_', '$AssignedTo_', '$Priority_')";
+		if(mysqli_query($conn,$insertchore_sql))
 		{
-			echo 'good';
+			//obtains the chore_id and uses user id so that we can add it to the user has chores db
 			$get_chore_id = "SELECT c_ID FROM chorechomper.chores WHERE chore_title = '$ChoreName_' AND complete_by_date = '$CompleteBy_' AND assigned_to = '$AssignedTo_' AND priority = '$Priority_'";
 			$c_result = mysqli_query($conn,$get_chore_id);
 			$row_c = mysqli_fetch_assoc($c_result);
 			$cid = $row_c['c_ID'];
-			$enter = "INSERT INTO chorechomper.user_has_chores (user_id, chore_id) VALUES ('$uid', '$cid')";
-			if(mysqli_query($conn,$enter))
-			{
-				echo 'good';
-			}
-			else
-				echo $uid . $cid;
+			$enter_into_relational = "INSERT INTO chorechomper.user_has_chores (user_id, chore_id) VALUES ('$uid', '$cid')";
+			mysqli_query($conn,$enter_into_relational);
 		}
 	}
 	CloseCon($conn);

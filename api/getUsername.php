a366bec83eafa297ec13974c1cc34e960a25f785
milelyk <44346861+milelyk@users.noreply.github.com>
2018-11-29 08:43:26 -0800

    Update getUsername.php

--- api/getUsername.php
+++ api/getUsername.php
@@ -1,9 +1,10 @@
 <?php
+	//this file gets the username of a user given the user id
 	include 'connection.php';
 	$conn = OpenCon();
 	$uid = $_GET["Id"];
-	$sql = ("SELECT Username FROM chorechomper.user WHERE user_id = '$uid'");
-	$result = mysqli_query($conn,$sql);
+	$getUsername_sql = ("SELECT Username FROM chorechomper.user WHERE user_id = '$uid'");
+	$result = mysqli_query($conn,$getUsername_sql);
 	
 	if($result->num_rows == 1)
 	{

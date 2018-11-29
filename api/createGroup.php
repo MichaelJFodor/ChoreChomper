3b181cbdabe7f1ca01a93d933d7ebf080a80b2ee
milelyk <44346861+milelyk@users.noreply.github.com>
2018-11-29 08:41:38 -0800

    Update createGroup.php

--- api/createGroup.php
+++ api/createGroup.php
@@ -1,12 +1,13 @@
 <?php
+	//this file takes in two parameters and creates a new group in the db
 	include 'connection.php';
 	$conn = OpenCon();	
 	$name = $_GET["Group_name"];
 	$password = $_GET["Group_password"];
 	
-	$sql = "INSERT into chorechomper.group (nameGroup, groupPassword) Values ('$name','$password')";
+	$create_group_sql = "INSERT into chorechomper.group (nameGroup, groupPassword) Values ('$name','$password')";
 	
-	if(mysqli_query($conn,$sql))
+	if(mysqli_query($conn,$create_group_sql))
 	{
 		echo json_encode('true');
 	}
